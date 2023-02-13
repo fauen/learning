cd97dbfe9a35da42fd27a5ba64afc736a95d280c
fauen <daniel@backman.io>
2023-02-13 12:32:27 +0100

    Updates

---
+++ installPC.ps1
@@ -0,0 +1,24 @@
+<#
+  .SYNOPSIS
+  Just a testing script.
+
+  .DESCRIPTION
+  We are using this part to learn and get a feeling of love in the universe.
+
+  .PARAMETER HelloThere
+  This should always respond with General Kenobi.
+
+  .PARAMETER Batman
+  RACHEL!
+
+  .INPUTS
+  None. You cannot pipe objects to installPC.ps1.
+
+  .OUTPUTS
+  None. installPC.ps1 does not generate any output.
+
+  .EXAMPLE
+  PS> .\installPC.ps1
+#>
+
+param ([string]$HelloThere, [string]$Batman)
