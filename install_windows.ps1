70ba77f6e65fc514cbcb48767331dda573b6ef8f
fauen <daniel@backman.io>
2023-02-13 07:37:35 +0100

    File moved from bin
    Moved the file from the bin repo as I'm converting my bash script to
    powershell.

---
+++ install_windows.ps1
@@ -0,0 +1,10 @@
+# Default applications to install when you have a new Windows install.
+wsl.exe --install
+winget install --id=Discord.Discord -e
+winget install --id=Microsoft.VisualStudioCode -e  
+winget install --id=7zip.7zip -e
+winget install --id=Mozilla.Firefox -e
+winget install --id=Microsoft.PowerToys -e  
+winget install --id=Spotify.Spotify -e  
+winget install --id=VideoLAN.VLC -e  
+winget install --id=SlackTechnologies.Slack -e
