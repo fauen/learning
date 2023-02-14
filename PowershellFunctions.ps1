a5f2e2dda04087b1b66c23ca114024dab363fe33
fauen <daniel@backman.io>
2023-02-14 11:16:34 +0100

    Updates only

--- PowershellFunctions.ps1
+++ PowershellFunctions.ps1
@@ -1,4 +1,4 @@
-function Update-GitHub {
+function Receive-GitHub {
     [CmdletBinding()]
     param (
     )
@@ -8,4 +8,40 @@ function Update-GitHub {
     else {
         Write-Verbose -Message "Git is not installed"
     }
+}
+
+function Read-GitHub {
+    [CmdletBinding()]
+    param (
+    )
+    if (Get-Command -Name git -CommandType Application) {
+        git status
+    }
+    else {
+        Write-Verbose -Message "Git is not installed"
+    } 
+}
+
+function Send-GitHub {
+    [CmdletBinding()]
+    param (
+    )
+    if (Get-Command -Name git -CommandType Application) {
+        git push
+    }
+    else {
+        Write-Verbose -Message "Git is not installed"
+    } 
+}
+
+function Write-GitHub {
+    [CmdletBinding()]
+    param (
+    )
+    if (Get-Command -Name git -CommandType Application) {
+        git add .; git commit; git push
+    }
+    else {
+        Write-Verbose -Message "Git is not installed"
+    } 
 }
