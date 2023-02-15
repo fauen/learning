4e2b952b998c198a5858a678610ebbd079748599
fauen <daniel@backman.io>
2023-02-15 08:27:49 +0100

    Updates

--- PowershellWindow.ps1
+++ PowershellWindow.ps1
@@ -8,6 +8,14 @@ $HelloWorldForm.ClientSize = '500,300'
 $HelloWorldForm.Text = 'Hello World!'
 $HelloWorldForm.BackColor = "#00FF00"
 
+$LblTitle = New-Object $LabelObject
+$LblTitle.Text = 'Hello World!'
+$LblTitle.AutoSize = $true
+$LblTitle.Font = 'Arial,24,style=Bold'
+$LblTitle.Location = New-Object System.Drawing.Point(20,20)
+
+$HelloWorldForm.Controls.AddRange(@($LblTitle))
+
 # Display the form
 $HelloWorldForm.ShowDialog()
 
